ff5721e1f5b2d7897033c25906073f819389eab8
Qambar Raza <qambar.raza@bbc.co.uk>
2018-12-18 15:31:00 +0000

    Update plugin to support custom messages

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ module.exports = testResults => {
     throw new Error("Please add a slack webhookUrl field under jestSlackReporter on your package.json");
   }
 
-  const errText = `<!here> Just a quick heads up, *${testResults.numFailedTests}* tests have failed :(
-  Please take a look. Peace`;
-
-  const passingText = `Sweet, all tests have passed`;
+  const errText = config.failMessage;
+  const passingText = config.passMessage;
 
   const text = testResults.numFailedTests > 0 ? errText : passingText;
 
